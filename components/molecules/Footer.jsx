29e34acff979a80f6438e410f33bbb00a9d472c3
jian <jian.kootker@gmail.com>
2022-10-31 23:59:53 +0100

    footer for desktop

--- components/molecules/Footer.jsx
+++ components/molecules/Footer.jsx
@@ -8,39 +8,47 @@ import { BsTwitter, BsLinkedin } from "react-icons/bs";
 export function Footer({ footer, page }) {
   return (
     <footer className="bg-secondary text-white ">
-      <div className=" flex justify-between pt-4 px-4 ">
+      <div className=" flex justify-between pt-4 px-4 lg:px-8 lg:pt-8">
         <div>
           <ul className="flex flex-col">
             <Link href="/#stories">
-              <a className="text-Phone/buttonlarge">{page[1].title}</a>
+              <a className="text-Phone/buttonlarge border-b w-fit mb-4 lg:mb-8 lg:text-Desktop/Label">
+                {page[1].title}
+              </a>
             </Link>
             <Link href="/about">
-              <a className="text-Phone/buttonlarge">{page[0].title}</a>
+              <a className="text-Phone/buttonlarge border-b w-fit lg:text-Desktop/Label">
+                {page[0].title}
+              </a>
             </Link>
           </ul>
           {/* contact us */}
-          <p className="pt-8 text-Phone/Label">{footer.contact}</p>
+          <p className="pt-8 text-Phone/Label lg:text-Desktop/Label">
+            {footer.contact}
+          </p>
           <Link href="/">
             <a>
-              <MdEmail className="h-6 w-6" />
+              <MdEmail className="h-6 w-6 lg:h-8 lg:w-8 lg:text-Desktop/Label" />
             </a>
           </Link>
           {/* follow us */}
-          <p className="pt-4 text-Phone/Label">{footer.volgOns}</p>
+          <p className="pt-4 text-Phone/Label lg:text-Desktop/Label lg:pt-8">
+            {footer.volgOns}
+          </p>
           <div className="flex pt-2">
             <Link href="/">
               <a className="pr-8">
-                <BsTwitter className="h-6 w-6" />
+                <BsTwitter className="h-6 w-6 lg:h-8 lg:w-8" />
               </a>
             </Link>
             <Link href="/">
               <a className="pr-8">
-                <BsLinkedin className="h-6 w-6" />
+                <BsLinkedin className="h-6 w-6 lg:h-8 lg:w-8" />
               </a>
             </Link>
             <Link href="/">
               <a className="pr-8">
-                <FiInstagram className="h-6 w-6" />
+                <FiInstagram className="h-6 w-6 lg:h-8 lg:w-8" />
               </a>
             </Link>
           </div>
@@ -58,7 +66,7 @@ export function Footer({ footer, page }) {
           ))}
         </div>
       </div>
-      <p className="pb-8 text-center pt-20 text-Phone/Labelsmall">
+      <p className="pb-8 text-center pt-20 text-Phone/Labelsmall lg:text-Desktop/Label lg:pt-0">
         {footer.copyrightText}
       </p>
     </footer>
